edb03281be71f9c5b356f93189f256b857a8d76d
Aleksey Frolov <wwwalexfrolov@mail.ru>
2020-05-07 16:54:10 +0300

    hotfix

--- config/application.rb
+++ config/application.rb
@@ -2,12 +2,11 @@ require_relative 'boot'
 
 require 'rails/all'
 
-config.assets.paths << Rails.root.join('node_modules')
-
 Bundler.require(*Rails.groups)
 
 module TaskManager
   class Application < Rails::Application
     config.load_defaults 6.0
+    config.assets.paths << Rails.root.join('node_modules')
   end
 end
