6222535036b4bf8f466fec506b41088471292345
Aleksey Frolov <wwwalexfrolov@mail.ru>
2020-05-12 00:22:59 +0300

    add check for developer's create

--- test/controllers/web/developers_controller_test.rb
+++ test/controllers/web/developers_controller_test.rb
@@ -6,13 +6,23 @@ class Web::DevelopersControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test 'should post create' do 
-    developer = create(:developer, attributes_for(:developer))
+  test 'should post create' do
+    new_developer = create(:developer)
     attrs = {
-      email: developer.email,
-      password: developer.password,
+      first_name: new_developer.first_name,
+      last_name: new_developer.last_name,
+      email: new_developer.email,
+      password: new_developer.password,
     }
+    post :create, params: { developer: attributes_for(:developer) }
+    assert_response :redirect
+
     post :create, params: { developer: attrs }
     assert_response :success
+    assert_not_nil new_developer
+    assert_not_empty attrs[:first_name]
+    assert_not_empty attrs[:last_name]
+    assert_not_empty attrs[:email]
+    assert_not_empty attrs[:password]
   end
 end
