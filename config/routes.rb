ba9ba08304f7e89a74fff03a3e6f9fa1f29e07fb
Aleksey Frolov <wwwalexfrolov@mail.ru>
2020-05-11 13:28:55 +0300

    first change

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root :to => 'web/boards#show'
 
   scope module: :web do
+    resources :developers, only: [:new, :create]
     resource :session, only: [:new, :create, :destroy]
     resource :board, only: :show
   end
