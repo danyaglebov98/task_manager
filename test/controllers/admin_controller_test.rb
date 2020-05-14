ad1b6adfaaeec5b5ef935c7a9af2b44389050956
Aleksey Frolov <wwwalexfrolov@mail.ru>
2020-05-14 16:33:31 +0300

    user list

--- test/controllers/admin_controller_test.rb
+++ test/controllers/admin_controller_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class Admin::UsersControllerTest < ActionController::TestCase
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
+  
   setup do
     admin = create(:admin)
     sign_in admin
