d10ddb1d83695628c5509b00bf07f0aea1b58695
Aleksey Frolov <wwwalexfrolov@mail.ru>
2020-05-15 11:44:49 +0300

    rework test + add seeds

--- test/controllers/admin_controller_test.rb
+++ test/controllers/admin_controller_test.rb
@@ -29,21 +29,30 @@ class Admin::UsersControllerTest < ActionController::TestCase
   end
 
   test 'should post create' do
-    user = attributes_for(:user)
-    post :create, params: { user: user }
+    attrs = attributes_for(:user)
+    post :create, params: { user: attrs }
+    current_user = User.find_by!(email: attrs[:email])
     assert_response :redirect
+    assert_equal current_user['email'], attrs[:email]
+    assert_equal current_user['first_name'], attrs[:first_name]
+    assert_equal current_user['last_name'], attrs[:last_name]
   end
 
   test 'should patch update' do
     user = create(:user)
-    user_attrs = attributes_for(:user)
-    patch :update, params: { id: user.id, user: user_attrs }
+    attrs = attributes_for(:user)
+    patch :update, params: { id: user.id, user: attrs }
+    patched_user = User.find_by!(email: attrs[:email])
     assert_response :redirect
+    assert_equal patched_user['email'], attrs[:email]
+    assert_equal patched_user['first_name'], attrs[:first_name]
+    assert_equal patched_user['last_name'], attrs[:last_name]
   end
 
   test 'should delete destroy' do
     user = create(:user)
     delete :destroy, params: {id: user.id }
     assert_response :redirect
+    assert_not User.exists?(user.id)
   end
 end
