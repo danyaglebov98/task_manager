a58f07029b08b8756393838a7c4c8aeebca45e1b
Aleksey Frolov <wwwalexfrolov@mail.ru>
2020-05-21 14:45:35 +0300

    install eslint & prettier

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,9 @@
-import 'material-design-lite/material.js';
-
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
+
+import 'material-design-lite/material.js';
+import WebpackerReact from 'webpacker-react'
+import TaskBoard from './components/TaskBoard'
+
+WebpackerReact.setup({ TaskBoard })
