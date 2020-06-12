7ad0eff7aa521cfe8333ae7bd90ab46982b14d09
Aleksey Frolov <wwwalexfrolov@mail.ru>
2020-06-12 14:57:45 +0300

    fix problems

--- app/javascript/presenters/TaskPresenter.js
+++ app/javascript/presenters/TaskPresenter.js
@@ -1,12 +1,14 @@
 import PropTypes from 'prop-types';
 import PropTypesPresenter from 'utils/PropTypesPresenter';
 
+import userPresenter from './UserPresenter';
+
 export default new PropTypesPresenter({
   id: PropTypes.number,
   name: PropTypes.string,
   description: PropTypes.string,
   state: PropTypes.string,
-  author: PropTypes.string,
-  assignee: PropTypes.string,
+  author: userPresenter.shape(),
+  assignee: userPresenter.shape(),
   transitions: PropTypes.object,
 });
--- lib/js_routes/js_routes.rake
+++ lib/js_routes/js_routes.rake
@@ -10,10 +10,7 @@ namespace :js_routes do
     file_name = File.join(ROUTES_DIR, 'ApiRoutes.js')
     JsRoutes.generate!(file_name, camel_case: true)
   end
-end
 
-namespace :js_routes do
-  desc 'Generate js routes for webpack'
   task generate: :environment do
     ROUTES_DIR = File.join('app', 'javascript', 'routes')
     FileUtils.mkdir_p(Rails.root.join(ROUTES_DIR))
